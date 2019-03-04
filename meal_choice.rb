3711b35041a0d38bed1bfc3151fd6f0b57d996fc
Riccardo <riccardo_london@live.it>
2019-03-04 16:42:08 +0000

    Done.

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,2 +1,5 @@
-# Define a method meal_choice that returns the meal_choice that was
+def meal_choice(diet)
+  return "meat"
+else return "#{diet}"
+  # Define a method meal_choice that returns the meal_choice that was
 # passed into it and defaults to meat.
